fccc65586281d900bf5447a5864b81e4e169fddb
worldown <d_dexter@mail.ru>
2016-12-04 23:12:37 +0300

    interfaces and implemetations have been created

---
+++ SeaFight/IObservable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaFight
+{
+    interface IObservable
+    {
+        void AddObserver(IObserver o);
+        void RemoveObserver(IObserver o);
+        void NotifyObservers(IObserver o);
+
+    }
+}
---
+++ SeaFight/Map.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaFight
+{
+
+    class Map : IObservable
+    {
+        public static int MAPSIZE = 12;
+        private List<IObserver> observers;
+        int[,] map;
+
+        public Map() {
+            observers = new List<IObserver>();
+        }
+        
+
+        public void AddObserver(IObserver o)
+        {
+            
+            observers.Add(o);
+        }
+
+        public void NotifyObservers()
+        {
+            foreach(IObserver ob in observers) {
+                ob.Update();
+            }
+        }
+
+        public void RemoveObserver(IObserver o)
+        {
+        }
+    }
+}
